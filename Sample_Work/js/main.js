245b882f49fc7468d32fbdb1eed761fae810fc9b
Michael Angelo <yamikamisama@gmail.com>
2015-02-02 22:25:01 -0500

    reload sample work

--- Sample_Work/js/main.js
+++ Sample_Work/js/main.js
@@ -6,29 +6,25 @@ $(document).ready(function() {
       slideSpeed : 300,
       paginationSpeed : 400,
       singleItem:true
-
-      // "singleItem:true" is a shortcut for:
-      // items : 1,
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
-
   });
+
   $("#bio_link").click(function(){
-    $("#center_carousel").children().hide();
+    $("#center_view").children().hide();
     $("#bio").fadeIn(1000);
   });
+
   $("#music_link").click(function(){
-    $("#center_carousel").children().hide();
+    $("#center_view").children().hide();
     $("#music").fadeIn(1000);
   });
-   $("#video_link").click(function(){
-    $("#center_carousel").children().hide();
+
+  $("#video_link").click(function(){
+    $("#center_view").children().hide();
     $("#video").fadeIn(1000);
   });
-    $("#pictures_link").click(function(){
-    $("#center_carousel").children().hide();
+
+  $("#pictures_link").click(function(){
+    $("#center_view").children().hide();
     $("#owl-demo").fadeIn(1000);
   });
 });
