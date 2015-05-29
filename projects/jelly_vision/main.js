2a1ad214fc198bfb7724fa7925b7702e0200743d
yamikamisama <yamikamisama@gmail.com>
2015-05-29 13:06:25 -0400

    add anscode to the questions

--- projects/jelly_vision/main.js
+++ projects/jelly_vision/main.js
@@ -12,46 +12,31 @@ $(document).ready(function() {
 
 	$('button').on('click', function(){
 		if($(window).width() > 1500) {
-			if ($(this).text() == "Challenge 1" ){
+			if ($(this).text() == "Part 1" ){
 				$('html, body').animate({scrollTop : 934},800);
 				return false;
-			} else if ( $(this).text() == "Challenge 2" ){
-				$('html, body').animate({scrollTop : 2473},800);
+			} else if ( $(this).text() == "Part 2" ){
+				$('html, body').animate({scrollTop : 3400},800);
 				return false;
-			} else if ( $(this).text() == "Challenge 3" ){
-				$('html, body').animate({scrollTop : 4150},800);
-				return false;
-			} else if ( $(this).text() == "Challenge 4" ){
-				$('html, body').animate({scrollTop : 5703},800);
-				return false;
-			} else if ( $(this).text() == "Challenge 5" ){
-				$('html, body').animate({scrollTop : 7556},800);
-				return false;
-			} else if ( $(this).text() == "Challenge 6" ){
-				$('html, body').animate({scrollTop : 10842},800);
+			} else if ( $(this).text() == "Part 3" ){
+				$('html, body').animate({scrollTop : 8401},800);
 				return false;
 			}
 		} else {
-			if ($(this).text() == "Challenge 1" ){
-				$('html, body').animate({scrollTop : 718},800);
+			if ($(this).text() == "Part 1" ){
+				$('html, body').animate({scrollTop : 894},800);
 				return false;
-			} else if ( $(this).text() == "Challenge 2" ){
-				$('html, body').animate({scrollTop : 2145},800);
+			} else if ( $(this).text() == "Part 2" ){
+				$('html, body').animate({scrollTop : 3293},800);
 				return false;
-			} else if ( $(this).text() == "Challenge 3" ){
-				$('html, body').animate({scrollTop : 3670},800);
-				return false;
-			} else if ( $(this).text() == "Challenge 4" ){
-				$('html, body').animate({scrollTop : 5103},800);
-				return false;
-			} else if ( $(this).text() == "Challenge 5" ){
-				$('html, body').animate({scrollTop : 6836},800);
-				return false;
-			} else if ( $(this).text() == "Challenge 6" ){
-				$('html, body').animate({scrollTop : 9970},800);
+			} else if ( $(this).text() == "Part 3" ){
+				$('html, body').animate({scrollTop : 8066},800);
 				return false;
 			}
 		}
 	});
 
 });
+
+
+// document.body.scrollTop
