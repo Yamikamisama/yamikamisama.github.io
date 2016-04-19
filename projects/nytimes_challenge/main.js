0e2f203c068192f028351660182702370e2919c8
Michael Angelo <yamikamisama@gmail.com>
2016-04-19 01:32:11 -0400

    add in martian language sswitch

--- projects/nytimes_challenge/main.js
+++ projects/nytimes_challenge/main.js
@@ -22,7 +22,7 @@ var NYTD = {
 
   renderArticles: function(){
     var divs = document.getElementsByClassName('article-holder');
-    for (i = 0; i < divs.length; ++i) {
+    for (var i = 0; i < divs.length; ++i) {
       var article = this.articleCollection[i]
       this.renderImageAndCredit(article, divs[i]);
       this.renderHeadline(article, divs[i]);
@@ -53,7 +53,8 @@ var NYTD = {
     headlineAnchor.href = article.url
     headline.innerHTML = article.headline;
     headline.setAttribute("class", "section-article-headline");
-    element.appendChild(headline)
+    element.appendChild(headlineAnchor)
+    headlineAnchor.appendChild(headline)
   },
 
   renderSummary: function(article, element){
@@ -68,6 +69,8 @@ var NYTD = {
     var bylineDate = document.createElement('SPAN');
     var bylineDivider = document.createElement('SPAN');
     var bylineAuthor = document.createElement('SPAN');
+    bylineDate.setAttribute("class", "byline-date text-left");
+    bylineAuthor.setAttribute("class", "byline-author text-left");
     bylineDate.innerHTML = new Date(article.lastPublished.slice(0, 19)).toString().slice(4, 15);
     bylineAuthor.innerHTML = article.byline;
     bylineDivider.setAttribute("class", "divider")
@@ -80,19 +83,45 @@ var NYTD = {
 
   changeText:function(element){
     var element = element.split(" ")
-    for(i = 0; i < element.length; i++){
-      element[i].length > 3 ? element[i] = "bazinga" : false
+    for(var i = 0; i < element.length; i++){
+      if(element[i].length > 3){
+        debugger
+        element[i].charAt(0) === element[i].charAt(0).toUpperCase() ? element[i] = "Boinga" : element[i] = "boinga"
+      }
     }
+    return element.join(" ")
   },
 
   initChangeLanguageListener: function(){
     document.getElementById("martian-button").addEventListener("click", function(event){
       event.preventDefault();
       var currentLang = document.getElementById("martian-button").innerHTML
+      var title = document.getElementById("title")
+      var credits = document.getElementsByClassName("section-image-credit")
+      var headlines = document.getElementsByClassName("section-article-headline")
+      var summaries = document.getElementsByClassName("section-article-summary")
+      var bylineDates = document.getElementsByClassName("byline-date")
+      var bylineAuthors = document.getElementsByClassName("byline-author")
+      var divs = document.getElementsByClassName('article-holder');
       if( currentLang === "Martian language"){
         document.getElementById("martian-button").innerHTML = "English language";
-
+        title.innerText = NYTD.changeText(title.innerText)
+        for (var i = 0; i < divs.length; i++) {
+          headlines[i].innerText = NYTD.changeText(headlines[i].innerText)
+          summaries[i].innerText = NYTD.changeText(summaries[i].innerText)
+          bylineDates[i].innerText = NYTD.changeText(bylineDates[i].innerText)
+          bylineAuthors[i].innerText = NYTD.changeText(bylineAuthors[i].innerText)
+        }
+      } else {
+        document.getElementById("martian-button").innerHTML = "Martian language";
+        title.innerText = "New York Times Coding Challenge";
+        for (var i = 0; i < divs.length; i++) {
+          headlines[i].innerHTML = NYTD.articleCollection[i].headline
+          summaries[i].innerHTML = NYTD.articleCollection[i].summary
+          bylineDates[i].innerHTML = new Date(NYTD.articleCollection[i].lastPublished.slice(0, 19)).toString().slice(4, 15);
+          bylineAuthors[i].innerHTML = NYTD.articleCollection[i].byline
       }
+    }
     });
   },
 
